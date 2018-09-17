5bb8251ea117c4af5c6f49350c49969436c162d9
Sumit Ranjan <sumit.ranjan819@gmail.com>
2018-09-17 12:18:52 +0530

    Updating the code
    Updating the code

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=assistant-bot-Web-Deploy.pubxml -p:Password=lgmZkzotXqaxSpJGoaDWDbR1lldowkQehvjd4pdlPi0ScYCfEuEsLx3qBwze
+
